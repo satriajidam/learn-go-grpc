b9c8d603b47fb766c9d365cb3ab6f3eaadb90240
Agastyo Satriaji Idam <agastyo@ruangguru.com>
2020-12-18 16:04:22 +0700

    Add gRPC server code

---
+++ pkg/protocol/grpc/server.go
@@ -0,0 +1,39 @@
+package grpc
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+
+	v1 "github.com/satriajidam/learn-go-grpc/pkg/api/v1"
+)
+
+func RunServer(ctx context.Context, v1API v1.ToDoServiceServer, port string) error {
+	listen, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return err
+	}
+
+	server := grpc.NewServer()
+	v1.RegisterToDoServiceServer(server, v1API)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			log.Println("shutting down gRPC server...")
+
+			server.GracefulStop()
+
+			<-ctx.Done()
+		}
+	}()
+
+	// start gRPC server
+	log.Println("starting gRPC server...")
+	return server.Serve(listen)
+}
